00fc3985829c5eae0cd2381163fb8d24524f1c7f
dz_paji <zhangqian2002@outlook.com>
2019-06-15 11:16:22 +0800

    Update b.sh

--- b.sh
+++ b.sh
@@ -1,3 +1,3 @@
-wget -qO- https://raw.githubusercontent.com/dz-paji/scripts/master/besttrace
+wget https://raw.githubusercontent.com/dz-paji/scripts/master/besttrace
 chmod +x besttrace
 besttrace -q 1 112.3.27.1
